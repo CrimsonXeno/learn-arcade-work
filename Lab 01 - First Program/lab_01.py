440b40c74788be9f414aa95b516d248ea20317c7
Daniel Everett <deeverett75@gmail.com>
2023-01-23 18:33:29 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,9 @@
-Hi
+print("Renee Rocco once said \"A hero will sacrifice the person they love to save the world, "
+      "but a villain will sacrifice the world to save the person they love\"")
+print("I picked this quote as it shows how from a different perspective a hero could be a "
+      "villain and vice versa")
+print("A example could be drawn from Batman in \"The Dark Knight\" in how he chose to save Harvey Dent versus "
+      "his love. He became her villain for the greater good as he believed Harvey could help put away the "
+      "criminals in a legal and beneficial way for Gotham")
+print("The quote just highlights how really a hero can be a villain and the opposite it is just a matter of"
+      "to whom.")
